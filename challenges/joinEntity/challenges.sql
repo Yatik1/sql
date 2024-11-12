f030dabdf6a7183898aa28099592de74e939d557
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-12 13:58:46 +0530

    challenges joins

--- challenges/joinEntity/challenges.sql
+++ challenges/joinEntity/challenges.sql
@@ -6,4 +6,21 @@ from customers c
 inner join employees e
 on c.salesRepEmployeeNumber = e.employeeNumber;
 
+-- 2. Report total payments for Atelier graphique.
+select c.customerName, sum(p.amount)
+from payments as p
+inner join customers as c
+on p.customerNumber = c.customerNumber
+where c.customerName="Atelier graphique"
+group by c.customerName;
+
+-- or 
+
+select c.customerName, sum(p.amount)
+from payments as p
+inner join customers as c
+on p.customerNumber = c.customerNumber
+group by c.customerName
+having c.customerName="Atelier graphique";
+
 
