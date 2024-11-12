103d27e12b340a023295272cff87202165206a91
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-12 13:58:34 +0530

    2

---
+++ challenges/joinEntity/Untitled.sql
@@ -0,0 +1,27 @@
+-- -------------------- SQL Joins Challenges ----------------------
+
+-- 1. Report the account representative for each customer.
+select customerName, concat(e.firstName, " " , e.lastName) as `Account Representative`
+from customers c
+inner join employees e
+on c.salesRepEmployeeNumber = e.employeeNumber;
+
+-- 2. Report total payments for Atelier graphique.
+select c.customerName, sum(p.amount)
+from payments as p
+inner join customers as c
+on p.customerNumber = c.customerNumber
+where c.customerName="Atelier graphique"
+group by c.customerName;
+
+-- or 
+
+select c.customerName, sum(p.amount)
+from payments as p
+inner join customers as c
+on p.customerNumber = c.customerNumber
+group by c.customerName
+having c.customerName="Atelier graphique";
+
+
+
